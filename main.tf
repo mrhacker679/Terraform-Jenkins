7792515e8c0b171703715a648ee4c76617dac2cb
mrhacker679 <93563893+mrhacker679@users.noreply.github.com>
2023-01-23 18:25:00 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
-provider "aws" {
-    region = "us-east-1"  
-}
+# provider "aws" {
+#     region = "us-east-1"  
+# }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
-  }
+# resource "aws_instance" "foo" {
+#   ami           = "ami-05fa00d4c63e32376" # us-west-2
+#   instance_type = "t2.micro"
+#   tags = {
+#       Name = "TF-Instance"
+#   }
+# }
+
+
+resource "local_file" "file01" {
+    filename = var.filename
+    content = var.content
 }
