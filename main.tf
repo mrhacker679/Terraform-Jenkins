99aceb95ed1c8f1c074a8b74dfce95c2a7c896e9
mrhacker679 <93563893+mrhacker679@users.noreply.github.com>
2023-10-09 17:09:45 -0500

    commented the aws code

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
-provider "aws" {
-    region = "us-east-1"  
-}
-
-resource "aws_instance" "foo" {
-  ami           = "ami-053b0d53c279acc90" # us-west-2
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
-  }
-}
-
-
-# resource "local_file" "file01" {
-#     filename = var.filename
-#     content = var.content
+# provider "aws" {
+#     region = "us-east-1"  
 # }
+
+# resource "aws_instance" "foo" {
+#   ami           = "ami-053b0d53c279acc90" # us-west-2
+#   instance_type = "t2.micro"
+#   tags = {
+#       Name = "TF-Instance"
+#   }
+# }
+
+
+resource "local_file" "file01" {
+    filename = var.filename
+    content = var.content
+}
